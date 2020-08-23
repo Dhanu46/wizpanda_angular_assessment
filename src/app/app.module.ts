16a58f1d5777ce2f67475d33b4c4b3fb9c3ae61f
Dhanu46 <dhanu.r46@gmail.com>
2020-08-23 11:13:45 -0700

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { ListComponent } from './list/list.component';
+import { UpdateComponent } from './update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    ListComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
